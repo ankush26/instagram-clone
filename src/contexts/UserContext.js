33e186a1e620fcf1e6068909f782e50c2397ff37
ankush26 <ankushgupta2601@gmial.com>
2021-10-03 23:49:54 +0530

    getUserData function completed in UserContext

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,23 +1,38 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import app from '../firebase';
-import { getFirestore, collection } from "firebase/firestore";
-
+import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
+import { useAuth } from '../contexts/AuthContext';
 
 const db = getFirestore(app)
 const UserContext = React.createContext()
 
-export function useUser(){
+export function useUser() {
     return useContext(UserContext)
 }
 
-export function UserProvider({children}) {
+export function UserProvider({ children }) {
+    const { currentUser } = useAuth();
+    const [userdata, setUserdata] = useState();
+    const [loading, setLoading] = useState(true)
 
-    const value={
+    
+    useEffect (async () => {
+        const q = query(collection(db, "users"), where("uid", "==", currentUser.uid));
 
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            setUserdata(doc.data())
+        });
+        console.log('ankush', currentUser.uid);
+        setLoading(false)
+    }, [])
+
+    const value = {
+        userdata,
     }
     return (
         <UserContext.Provider value={value}>
-            {children}
+            {!loading && children}
         </UserContext.Provider>
     )
 }
